e3d0f516eff18eeb9560e7282741018dcd8fd74b
lvxingzhi <miaocheng2021>
2021-08-03 14:29:13 +0800

    [1.0][优化] 项目拆分

---
+++ stcconfig-client/src/main/java/cn/notenextday/stcconfigclient/StcconfigClientApplication.java
@@ -0,0 +1,13 @@
+package cn.notenextday.stcconfigclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StcconfigClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StcconfigClientApplication.class, args);
+    }
+
+}
---
+++ stcconfig-server/src/main/java/cn/notenextday/stcconfigserver/StcconfigServerApplication.java
@@ -0,0 +1,13 @@
+package cn.notenextday.stcconfigserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StcconfigServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StcconfigServerApplication.class, args);
+	}
+
+}
