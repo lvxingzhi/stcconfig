09f2c14c52a480dcb8afc008ccbee2d264d068ae
lvxingzhi <miaocheng2021>
2021-12-01 18:43:37 +0800

    [1.0][web] 添加WEB管理项目

---
+++ stcconfig-web/src/main/java/cn/notenextday/stcconfigweb/StcconfigWebApplication.java
@@ -0,0 +1,13 @@
+package cn.notenextday.stcconfigweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StcconfigWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StcconfigWebApplication.class, args);
+    }
+
+}
---
+++ stcconfig-web/src/test/java/cn/notenextday/stcconfigweb/StcconfigWebApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.notenextday.stcconfigweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StcconfigWebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
